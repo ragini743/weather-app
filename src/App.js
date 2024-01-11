c7fc01f5459e7f654f6d598b8a0858ce6bb56729
Ragini <raginiv743@gmail.com>
2024-01-11 20:33:38 +0530

    fix error

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import WeatherLayout from "./components/WeatherLayout";
 
-
-
 function App() {
   return (
     <div className="flex justify-center items-center h-[80vh] my-14 ">
    
      <WeatherLayout />
-    </div>
+         </div>
   );
 }
 

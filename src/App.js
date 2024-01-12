21db56c58a18b0e8eb2fc8b7f26608f4e14daaf0
Ragini <raginiv743@gmail.com>
2024-01-12 21:07:31 +0530

    reverted the changes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,4 @@
-import React from "react";
-import { useState } from "react";
-
-import SearchContainer from "./components/SearchContainer";
-import WeatherReport from "./components/WeatherReport";
-
-const WeatherLayout = () => {
-  const [weatherData, setWeatherData] = useState(null);
-
-  return (
-    <div
-      className="bg-indigo-600
-   text-white py-8 sm:w-[80%] md:w-[60%] mx-auto h-full px-12 md:py-10 max-w-md rounded-3xl border-gray-300 border-2  shadow-2xl"
-    >
-      <SearchContainer
-        weatherData={weatherData}
-        setWeatherData={setWeatherData}
-      />
-
-      <WeatherReport weatherData={weatherData} />
-    </div>
-  );
-};
+import WeatherLayout from "./components/weatherLayout";
 
 function App() {
   return (
